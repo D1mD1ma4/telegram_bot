69f003258d7215f41adc4a97fd7806365945fa72
D1mD1ma4 <kdima7812@gmail.com>
2025-02-28 22:12:09 +0300

    addedBOT

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-print('Hello world')
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+
+def get_token_from_file():
+    with open("token.txt", "r") as file:
+        token = file.read().strip()
+    return token
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Привет! Я твой новый бот.')
+
+
+def main():
+    token = get_token_from_file()
+
+    application = Application.builder().token(token).build()
+
+    application.add_handler(CommandHandler("start", start))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
